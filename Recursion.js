271efa76450623715b3118cab1e22efa5aeb64bb
VISHAL SINGH <111185144+daggercoding@users.noreply.github.com>
2024-04-16 20:45:45 +0530

    Add files via upload
    Recursion

---
+++ Recursion.js
@@ -0,0 +1,17 @@
+(function(n, m) {
+  // body...
+  if (n <= m) {
+    console.log(n);
+    n++;
+    (n, m);
+  }
+})(1, 10);
+
+ (function(n, m) {
+      // body...
+      while (n <= m) {
+        console.log(n);
+        n++;
+        (n, m);
+      }
+    })(1, 10);
